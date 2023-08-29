bc2a5f2dbbf83588c0fde888b376946a527de98d
harmanmstf <harmanmstf@gmail.com>
2023-08-29 15:02:09 +0300

    home ui done

--- app/src/main/java/com/example/babytracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/babytracker/ui/home/HomeFragment.kt
@@ -1,39 +1,33 @@
-package com.example.babytracker.ui.onboarding
+package com.example.babytracker.ui.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.babytracker.R
+import com.example.babytracker.databinding.FragmentHomeBinding
 import com.example.babytracker.databinding.FragmentOnboarding1Binding
-import com.example.babytracker.databinding.FragmentOnboarding2Binding
 
+class HomeFragment : Fragment() {
 
-class Onboarding2Fragment : Fragment() {
-
-    private var _binding: FragmentOnboarding2Binding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        _binding = FragmentOnboarding2Binding.inflate(inflater, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.btnNext.setOnClickListener {
-            findNavController().navigate(R.id.action_onboarding2Fragment_to_homeFragment)
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        _binding = null!!
     }
 }
