7d6cf595a00371aa96eb45ecfe59e325bf42689d
harmanmstf <harmanmstf@gmail.com>
2023-09-06 14:31:23 +0300

    manifest done

--- app/src/main/java/com/example/babytracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/babytracker/ui/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.babytracker.ui
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
 import com.example.babytracker.R
+import com.revenuecat.purchases.LogLevel
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
 import dagger.hilt.android.AndroidEntryPoint
+import com.example.babytracker.util.GOOGLE_API_KEY
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Purchases.logLevel = LogLevel.DEBUG
+        Purchases.configure(PurchasesConfiguration.Builder(this, GOOGLE_API_KEY).build())
     }
 }
---
+++ app/src/main/java/com/example/babytracker/util/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.babytracker.util
+
+
+const val GOOGLE_API_KEY = "goog_ZebdFOeWbuuxfTPffVGIZcLCRhR"
